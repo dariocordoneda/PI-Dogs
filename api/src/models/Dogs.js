298d69db06bf87f6d4fa0a79197867765f01c64e
DarioCordoneda <dario_cordoneda@hotmail.com>
2023-05-31 20:28:23 -0300

    Tabla de DOGS creada

---
+++ api/src/models/Dogs.js
@@ -0,0 +1,52 @@
+const { DataTypes } = require('sequelize');
+
+const { v4:uuidv4 } = require('uuid');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  const Dogs = sequelize.define('Dogs', {
+    ID: {
+      type: DataTypes.UUIDV4,
+      defaultValue: uuidv4(),
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    height: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    weight: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    life_span: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  });
+
+  return Dogs;
+
+  
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
